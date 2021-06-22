ee3874d89d8f33efa089bb3627c6e6b9f10a9553
Ajinkya9881 <85883509+Ajinkya9881@users.noreply.github.com>
2021-06-22 21:23:22 +0530

    Add files via upload

---
+++ 01 Given Number is Even or Odd-Using if else.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+int main()
+{
+  int Num =432;
+  
+  if(Num %2 ==0)
+  {
+       printf("\n  Number %d is Even .",Num);
+  }
+  else
+  {
+       printf("\n Number %d is Odd.",Num);
+  }
+   
+  printf("\n\n Thanks!!!");
+  
+  getch();
+  return 0;
+}
