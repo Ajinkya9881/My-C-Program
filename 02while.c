817143d0eea7eed3fe3aa6dac68e019807f6390e
Ajinkya9881 <85883509+Ajinkya9881@users.noreply.github.com>
2021-06-22 21:26:33 +0530

    Add files via upload

---
+++ 02while.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int i=1;
+  
+  while(i<=5)
+  {
+    printf("\n INDIA");
+    printf("\n I LOVE INDIA");
+    i++;
+   }
+  
+    printf("\n Thanks!!!");
+    
+    getch();
+    return 0;
+}
