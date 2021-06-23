08425d286081ac3558a7ee584087dc357961dd2e
Ajinkya9881 <85883509+Ajinkya9881@users.noreply.github.com>
2021-06-23 11:33:08 +0530

    Add files via upload

---
+++ 01 To cheack Given Number is Positve or Negative -Using if else.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+int main()
+{
+      int Num =25;
+      
+      if(Num>0)
+      {
+            printf("\n Number %d is Positive.",Num);
+      }
+      else 
+      {    
+            printf("\n Number %d is Negative.",Num);
+      }
+      printf ("\n Bye Bye!!! \n\n Press Any Key To Quite!!!");
+      
+      getch();
+            
+      return 0;
+}
