a31fbb536b9fa55e3b17159cc796c4d26866a665
Ajinkya9881 <85883509+Ajinkya9881@users.noreply.github.com>
2021-06-23 09:49:36 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+int main() 
+{
+
+         int i=0;
+         
+         do
+         {
+            printf("\n INDIA");
+            i++;
+         }
+         while(i<5);
+            
+            printf("\n\nThanks!!!");
+            
+         getch(); 
+         return 0;
+} 
