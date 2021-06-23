8a9d2af69235acdbb46f9651de93741c0276dab6
Ajinkya9881 <85883509+Ajinkya9881@users.noreply.github.com>
2021-06-23 12:34:09 +0530

    Add files via upload

---
+++ 02 Given Number is Positive Negative-Using if else.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+int main()
+{
+  
+        int Num = 0;
+        
+        printf("\n Enter an Integer Number To Check Wheather it is Positive or Negative:");
+        scanf("%d",&Num);
+        
+        if(Num == 0)
+        {
+               
+               printf("\n Given Number %d is Neutral.",Num);
+         
+        }
+        else if(Num>0)
+        {
+          
+               printf("\n Number %d is Positive .",Num);
+         
+        }
+        else
+        {
+               printf("\n Number %d is Negative.",Num);
+        }
+               
+         printf("\n Bye Bye!!! \n\n Press Any Key To Quite!!!");
+        
+        getch();
+        
+        return 0;
+}
